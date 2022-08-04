f9a7898e1487ba57aa5865753323ea3daa61f6a9
Mamonamoli Mpopo <mamonamoli.mpopo@africacode.academy>
2022-08-04 07:16:05 +0100

    added complete files

---
+++ tik-tac-tok-game/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TikTacToe from './TicTacToe/TikTacToe';
+
+function App() {
+  return (
+    <div className="App">
+      <TikTacToe/>
+    </div>
+  );
+}
+
+export default App;
